7dbf33dd8ea092a1339af0fa51467c9d4b893cf5
brenoalberto <brenoaps435@gmail.com>
2020-06-30 07:04:15 -0300

    update package.json and add noise

---
+++ Learning/noise/1.js
@@ -0,0 +1,60 @@
+const { lerp } = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
+const palettes = require("nice-color-palettes");
+
+export const settings = {
+  dimensions: [2048, 2048],
+};
+
+export const sketch = () => {
+  const colorCount = random.rangeFloor(1, 6);
+  const palette = random.shuffle(random.pick(palettes)).slice(0, colorCount);
+
+  const createGrid = () => {
+    const points = [];
+    const count = 30;
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+        const radius = Math.abs(random.noise2D(u, v)) * 0.1;
+        points.push({
+          color: random.pick(palette),
+          radius,
+          rotation: random.noise2D(u, v),
+          position: [u, v],
+        });
+      }
+    }
+    return points;
+  };
+
+  const points = createGrid().filter(() => random.value() > 0.4);
+  const margin = 400;
+
+  return ({ context, width, height }) => {
+    context.fillStyle = "white";
+    context.fillRect(0, 0, width, height);
+
+    points.forEach((data) => {
+      const { position, radius, color, rotation } = data;
+      const [u, v] = position;
+
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, width - margin, v);
+
+      //   context.beginPath();
+      //   context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      //   context.fillStyle = color;
+      //   context.fill();
+
+      context.save();
+      context.fillStyle = color;
+      context.font = `${radius * width}px 'Helvetica'`;
+      context.translate(x, y);
+      context.rotate(rotation);
+      context.fillText("=", 0, 0);
+      context.restore();
+    });
+  };
+};
